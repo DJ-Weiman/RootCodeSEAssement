c728a08bfc39e3df27c9176ccc64df29325fa92c
Dion Weiman <dionweiman1998@gmail.com>
2025-04-10 18:58:47 +0530

    Initial Commit

---
+++ src/main/java/com/djw/rootcodeSEAssessment/RootcodeSeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.djw.rootcodeSEAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootcodeSeAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootcodeSeAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djw/rootcodeSEAssessment/RootcodeSeAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djw.rootcodeSEAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootcodeSeAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
